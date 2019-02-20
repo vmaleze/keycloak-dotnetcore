453f95165e7a7aa759d316cd7682d8e6de98ecda
Vivien Maleze <vivien.maleze@thermofisher.com>
2019-02-20 13:36:33 +0100

    Init repo

---
+++ KeycloakDemo/Controllers/SecuredController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KeycloakDemo.Controllers
+{
+    [Authorize]
+    [Route("/secured")]
+    [ApiController]
+    public class SecuredController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Secured()
+        {
+            return Ok($"You are authenticated as : {User.FindFirst("preferred_username")?.Value}");
+        }
+        
+        [HttpGet("dumb")]
+        [Authorize(Roles = "ROLE_DUMB")]
+        public IActionResult Dumb()
+        {
+            return Ok("You should not be able to access this ...");
+        }
+        
+        [HttpGet("role")]
+        [Authorize(Roles = "yes_we_can")]
+        public IActionResult Role()
+        {
+            return Ok("That's right, you can");
+        }
+    }
+}
